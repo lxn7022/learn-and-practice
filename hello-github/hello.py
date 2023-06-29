04019250156b85a87edbc0c4bdd6af85e051da39
larkliu <larkliu@tencent.com>
2023-06-29 17:49:08 +0800

    resolve conflict

--- hello-github/hello.py
+++ hello-github/hello.py
@@ -3,6 +3,7 @@ def hello(name: str):
     print("Hello World: ", name)
 
 
+<<<<<<< HEAD
 def world():
     print("change the world")
 
@@ -11,5 +12,7 @@ def foo():
     print("this is a function: foo()")
 
 
+=======
+>>>>>>> 4ddd89dae4549c6290bd2d69ba3cac3b31fd881c
 if __name__ == '__main__':
     hello('haha')

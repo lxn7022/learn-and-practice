ecb0790bbe3c0018ff1ce1e2d2f8255fb08d658c
larkliu <larkliu@tencent.com>
2023-06-29 17:57:17 +0800

    add a new dir

---
+++ hello-github/hello.py
@@ -0,0 +1,15 @@
+
+def hello(name: str):
+    print("Hello World: ", name)
+
+
+def world():
+    print("change the world")
+
+
+def foo():
+    print("this is a function: foo()")
+
+
+if __name__ == '__main__':
+    hello('haha')

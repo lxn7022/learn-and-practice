4ba91af5e5f81b1bbc2922be08bfaeab2afc9f34
lxn7022 <xnliu@qq.com>
2023-07-24 18:40:15 +0800

    feat: add c++ code framework

---
+++ c++/test/main.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <string.h>
+
+using namespace std;
+
+void func()
+{
+}
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
